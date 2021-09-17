8a348dc3ab361448a4bcfe98a5dee08111d8fac3
Turlough Mullan <duke1199sp@gmail.com>
2021-09-17 15:30:46 +0200

    Kinda cleans up after itself now

--- src/vector/vector.hpp
+++ src/vector/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: tmullan <tmullan@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/14 17:07:27 by tmullan       #+#    #+#                 */
-/*   Updated: 2021/09/17 12:49:06 by tmullan       ########   odam.nl         */
+/*   Updated: 2021/09/17 15:30:26 by tmullan       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,13 @@ class vector
 {
 	public:
 
-		typedef	Alloc		allocator_type;
-		typedef	T			value_type;
+		typedef				Alloc							allocator_type;
+		typedef				T								value_type;
+		typedef typename	allocator_type::reference		reference;
+		typedef typename	allocator_type::const_reference	const_reference;
+		typedef	typename	allocator_type::pointer			pointer;
+		typedef	typename	allocator_type::const_pointer	const_pointer;
+
 
 		vector<T, Alloc>() : capacity(10) , size(0) {
 			array = allocator.allocate(10); // Set it to allocate 10 randomly for now
@@ -33,7 +38,7 @@ class vector
 			array = allocator.allocate(alloc_size);
 		}
 		~vector<T, Alloc>() {
-			// allocator.deallocate(array, 10);
+			allocator.deallocate(array, capacity);
 		}
 		
 		void	push_back(T arg) {
@@ -54,14 +59,12 @@ class vector
 		size_t			size;
 
 		void	re_size() {
-			std::cout << "welcome welcome welcome" << std::endl;
 			T	*temp = allocator.allocate(capacity * 2);
 			for (size_t i = 0; i < size; i++)
 				temp[i] = array[i];
 			allocator.deallocate(array, capacity);
 			capacity *= 2;
 			array = temp;
-			std::cout << capacity << std::endl;
 		}
 };
 
--- src/vector_main.cpp
+++ src/vector_main.cpp
@@ -6,7 +6,7 @@
 /*   By: tmullan <tmullan@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/14 16:44:37 by tmullan       #+#    #+#                 */
-/*   Updated: 2021/09/17 12:48:23 by tmullan       ########   odam.nl         */
+/*   Updated: 2021/09/17 14:49:54 by tmullan       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,9 @@ int		main(void) {
 	fuck.push_back(69);
 	fuck.push_back(99);
 	fuck.push_back(7);
+	fuck.push_back(69);
+	fuck.push_back(99);
+	fuck.push_back(865);
 
 	std::cout << fuck.back() << std::endl;
 }
