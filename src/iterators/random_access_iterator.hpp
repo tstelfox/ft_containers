1f07b1705c0b7a1e2046980a00cac95fdb424381
Turlough Mullan <duke1199sp@gmail.com>
2021-09-29 12:14:51 +0200

    Doing reading and watching videos on iterators

--- src/iterators/random_access_iterator.hpp
+++ src/iterators/random_access_iterator.hpp
@@ -6,19 +6,22 @@
 /*   By: tmullan <tmullan@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/28 17:23:20 by tmullan       #+#    #+#                 */
-/*   Updated: 2021/09/28 17:57:37 by tmullan       ########   odam.nl         */
+/*   Updated: 2021/09/29 11:43:05 by tmullan       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+#include <iterator>
+
 namespace ft {
 
-template <typename T, class Category = std::random_access_iterator_tag>
-class random_access_iterator {
+template <class value_type, class difference_type, 
+		class pointer, class reference, class iterator_category = std::random_access_iterator_tag>
+class v_iterator {
 
 	public:
-		
+		v_iterator() {}	
 };
 
 }
--- src/vector/vector.hpp
+++ src/vector/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: tmullan <tmullan@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/14 17:07:27 by tmullan       #+#    #+#                 */
-/*   Updated: 2021/09/28 17:43:56 by tmullan       ########   odam.nl         */
+/*   Updated: 2021/09/29 11:27:09 by tmullan       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,8 @@ class vector
 		typedef const		value_type&						const_reference;
 		typedef				value_type*						pointer;
 		typedef	const		value_type*						const_pointer;
-		// typedef				ft::iterator					iterator;
+		// typedef				std::random_access_iterator_tag					iterator;
+		typedef				ptrdiff_t						difference_type;
 		typedef				size_t							size_type;
 
 		explicit vector(const allocator_type &alloc = allocator_type()) : allocator(alloc) , v_capacity(10) , v_size(0) {
