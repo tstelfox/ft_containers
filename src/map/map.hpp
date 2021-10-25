5dbe7c6e9987ec9ea94ec6efdd9e6014963c3496
Turlough Mullan <duke1199sp@gmail.com>
2021-10-25 12:35:25 +0200

    Begun typedefing

--- src/map/map.hpp
+++ src/map/map.hpp
@@ -6,7 +6,7 @@
 /*   By: tmullan <tmullan@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/14 17:27:29 by tmullan       #+#    #+#                 */
-/*   Updated: 2021/10/25 12:26:42 by tmullan       ########   odam.nl         */
+/*   Updated: 2021/10/25 12:35:07 by tmullan       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,15 @@ template < class Key, class T, class Compare = std::less<Key>, class Alloc = std
 class map
 {
 	public:
+
+		typedef	Key										key_type;
+		typedef	T										mapped_type;
+		typedef std::pair<const key_type, mapped_type>	value_type;
+
+
 		map() {}
 		~map() {}
 	private:
-		/* data */
+		Alloc	m_allocator;
+
 };
