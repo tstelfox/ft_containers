af2496b8b2e33c9b8fb7e63d5ca16517c164778b
Turlough Mullan <duke1199sp@gmail.com>
2021-10-25 12:26:08 +0200

    Alright starting Map

--- src/map/map.hpp
+++ src/map/map.hpp
@@ -6,12 +6,15 @@
 /*   By: tmullan <tmullan@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/14 17:27:29 by tmullan       #+#    #+#                 */
-/*   Updated: 2021/09/14 17:28:04 by tmullan       ########   odam.nl         */
+/*   Updated: 2021/10/25 12:25:43 by tmullan       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
 #include <iostream>
+#include <memory>
 
+template < class Key, class T, class Compare = std::less<Key>, class Alloc = std::allocator< std::pair<const Key, T> > >
 class map
 {
 private:
