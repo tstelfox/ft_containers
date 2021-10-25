f55e0aa1d83791e5524dac1e2cc4a2282147332d
Turlough Mullan <duke1199sp@gmail.com>
2021-10-25 16:33:03 +0200

    Trying to work out the basics of the map

--- src/map/map.hpp
+++ src/map/map.hpp
@@ -6,13 +6,14 @@
 /*   By: tmullan <tmullan@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/14 17:27:29 by tmullan       #+#    #+#                 */
-/*   Updated: 2021/10/25 12:35:07 by tmullan       ########   odam.nl         */
+/*   Updated: 2021/10/25 16:32:44 by tmullan       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <iostream>
 #include <memory>
+#include <map>
 
 template < class Key, class T, class Compare = std::less<Key>, class Alloc = std::allocator< std::pair<const Key, T> > >
 class map
@@ -22,11 +23,24 @@ class map
 		typedef	Key										key_type;
 		typedef	T										mapped_type;
 		typedef std::pair<const key_type, mapped_type>	value_type;
+		typedef	Compare									key_compare;
+		typedef	Alloc									allocator_type;
+		typedef	value_type&								reference;
+		typedef	const value_type&						const_reference;
+		typedef value_type*								pointer;
+		typedef const value_type*						const_pointer;
+		// Iterator stuff
+		typedef	size_t									size_type;
+		typedef	ptrdiff_t								difference_type;
 
+		// Somehow have to build this value_compare
 
 		map() {}
 		~map() {}
 	private:
-		Alloc	m_allocator;
+		allocator_type	m_allocator;
+		key_compare		_comp;
+		
+		
 
 };
