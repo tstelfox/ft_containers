7bb4f0e9df2d5372f2ac1e483c3c4ce167190e49
Turlough Mullan <duke1199sp@gmail.com>
2021-10-25 12:27:03 +0200

    Getting hungry though

--- src/map/map.hpp
+++ src/map/map.hpp
@@ -6,7 +6,7 @@
 /*   By: tmullan <tmullan@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/14 17:27:29 by tmullan       #+#    #+#                 */
-/*   Updated: 2021/10/25 12:25:43 by tmullan       ########   odam.nl         */
+/*   Updated: 2021/10/25 12:26:42 by tmullan       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,9 @@
 template < class Key, class T, class Compare = std::less<Key>, class Alloc = std::allocator< std::pair<const Key, T> > >
 class map
 {
-private:
-	/* data */
-public:
-	map(/* args */);
-	~map();
+	public:
+		map() {}
+		~map() {}
+	private:
+		/* data */
 };
-
-map::map(/* args */)
-{
-	std::cout << "The map is here" << std::endl;
-}
-
-map::~map()
-{
-}
