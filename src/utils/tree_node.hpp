dc8e677aa83637185ff976a1a4fedd308b1351f4
Turlough Mullan <duke1199sp@gmail.com>
2021-11-01 16:32:09 +0100

    Node is maybe making more sense

--- src/utils/tree_node.hpp
+++ src/utils/tree_node.hpp
@@ -6,7 +6,7 @@
 /*   By: tmullan <tmullan@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/29 13:50:54 by tmullan       #+#    #+#                 */
-/*   Updated: 2021/11/01 16:08:32 by tmullan       ########   odam.nl         */
+/*   Updated: 2021/11/01 16:30:27 by tmullan       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,17 @@ template < class value_type >
 class node {
 
 	public:
+		node*		parent;
+		node*		left;
+		node*		right;
+
+		value_type	object;
+
+
 		node() {}
 
 		~node() {}
 		
 	private: // Maybe (?)
-		node*		left;
-		node*		right;
-
-		value_type	object;
 		
 };
