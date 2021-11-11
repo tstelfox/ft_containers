68933bc72283279bbf455852800f916e99035bdf
Turlough Mullan <duke1199sp@gmail.com>
2021-11-11 18:02:04 +0100

    Yea, next job needs to be to go up and down the binary tree

--- src/map_main.cpp
+++ src/map_main.cpp
@@ -6,7 +6,7 @@
 /*   By: tmullan <tmullan@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/15 17:04:58 by tmullan       #+#    #+#                 */
-/*   Updated: 2021/11/11 17:44:06 by tmullan       ########   odam.nl         */
+/*   Updated: 2021/11/11 17:57:01 by tmullan       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,16 @@ void	test_basic() {
 
 	// std::pair<ft::map<int, std::string>::iterator, bool> deh = first_v.insert(std::pair<int, std::string>(99, "Mazzarri"));
 	first_v.insert(std::pair<int, std::string>(99, "Mazzarri"));
-	std::pair<ft::map<int, std::string>::iterator, bool> deh = first_v.insert(std::pair<int, std::string>(47, "Dio Vigliacco"));
+	// std::pair<ft::map<int, std::string>::iterator, bool> deh = first_v.insert(std::pair<int, std::string>(47, "Dio Vigliacco"));
 	ft::map<int, std::string>::iterator it = first_v.begin();
 
 	std::cout << it->object.second << std::endl;
-	// it = deh.first;
-	it++;
-	std::cout << it->object.second << std::endl;
+	// it++; // This requires the nodes and iterators to go up the tree
+	
+	//The following isn't working diobe:
+	// node<std::pair<const int, std::string> > next = *it->left;
+	// it = it->right;
+	// std::cout << next.object.second << std::endl;
 	
 }
 
