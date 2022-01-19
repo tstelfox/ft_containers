f9a570e718bfeb55072951d6bda3cd917b014844
Turlough Mullan <duke1199sp@gmail.com>
2022-01-19 17:27:44 +0100

    cannae be fucked anymore

---
+++ src/utils/cpp_eleven_shiz.hpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   cpp_eleven_shiz.hpp                                :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: tmullan <tmullan@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/01/19 17:23:34 by tmullan       #+#    #+#                 */
+/*   Updated: 2022/01/19 17:27:07 by tmullan       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+template <class T>
+struct is_integral
+{
+	// I REALLY don't get the point of this
+};
