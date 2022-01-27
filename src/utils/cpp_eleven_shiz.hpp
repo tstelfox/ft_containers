0f0898938e2a79d73a434b2bc3e9dec45d98b734
Codemuncher <duke1199sp@gmail.com>
2022-01-27 11:59:26 +0100

    Ubunutu doesn't like unicode

--- src/utils/cpp_eleven_shiz.hpp
+++ src/utils/cpp_eleven_shiz.hpp
@@ -23,8 +23,8 @@ template <>          struct is_integral<char>                { static const bool
 template <>          struct is_integral<signed char>         { static const bool value = true; };
 template <>          struct is_integral<unsigned char>       { static const bool value = true; };
 template <>          struct is_integral<wchar_t>             { static const bool value = true; };
-template <>          struct is_integral<char16_t>            { static const bool value = true; };
-template <>          struct is_integral<char32_t>            { static const bool value = true; };
+// template <>          struct is_integral<char16_t>            { static const bool value = true; };
+// template <>          struct is_integral<char32_t>            { static const bool value = true; };
 template <>          struct is_integral<short>               { static const bool value = true; };
 template <>          struct is_integral<unsigned short>      { static const bool value = true; };
 template <>          struct is_integral<int>                 { static const bool value = true; };
